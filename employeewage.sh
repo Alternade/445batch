d4e122a7c0a248c30338f819d6bea4a2c414e9a3
Alternade <aptsahu2@gmail.com>
2022-02-08 09:19:26 +0530

    Added Daily wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,10 +1,15 @@
+ratePrHr=20
 isPresent=1
 check=$((RANDOM%2))
 
 if [ $isPresent -eq $check ]
 then 
 	echo "Employee is present"
+	wrkHrs=8
 else
 	echo "Employee is absent"
+	wrkHrs=0
 fi
+salary=$(($ratePrHr*$wrkHrs))
+echo "Salary is $salary"
 
